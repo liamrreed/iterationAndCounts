81ef42bd9d23a613c826317d87ec0ccc20e9744f
Liam Reed <132017174+liamrreed@users.noreply.github.com>
2023-04-27 14:35:56 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+'''
+for number in range(1,20):
+    print(number)
+print("\n")
+for number in range(20,0,-1):
+    print(number)
+print("\n")
+for number in range(2,12,2):
+    print(number)
+print("\n")
+for number in range(9,0,-2):
+    print(number)
+'''
+
+x = 1
+while (x < 6):
+  print(x)
+  x = x + 1
+print("\n")
+x = 10
+while (x > 0):
+  print(x)
+  x = x - 1
+print("\n")
+x = 0
+while (x < 10):
+    x += 2
+    print(x)
+print("\n")
+x = 11
+while (x > 1):
+    x -= 2
+    print(x)
